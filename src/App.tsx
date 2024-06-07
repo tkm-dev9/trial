0eb7b07ec1d25c486b296f9acd73bcf317a5d85e
takumu1011 <sumikawa.web@gmail.com>
2024-06-08 01:19:59 +0900

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div>App</div>;
+  return <div className="text-3xl text-red-400 line-through p-10">App</div>;
 }
 
 export default App;
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
